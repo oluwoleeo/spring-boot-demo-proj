5e29f9aca30e665fc05f7c454fb1c15cfa8e8225
Oluwaremi Oluwole <polymathwole@gmail.com>
2023-10-04 08:21:52 +0100

    Using environment variables

--- src/main/java/com/wole/jpademo/controller/PersonController.java
+++ src/main/java/com/wole/jpademo/controller/PersonController.java
@@ -1,13 +1,21 @@
 package com.wole.jpademo.controller;
 
 import com.wole.jpademo.dto.PersonDTO;
+import com.wole.jpademo.dto.SimpleMessage;
 import com.wole.jpademo.entity.Person;
 import com.wole.jpademo.mapper.PersonMapper;
 import com.wole.jpademo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +26,21 @@ public class PersonController {
 
     @Autowired
     PersonService personService;
+
+    // @Value("${random.value}")
+    // @Value("${random.long}")
+    // @Value("${random.uuid}")
+    // @Value("${random.int(10)}")
+    // @Value("${random.int[25,36]}")
+    @Value("${simple.message}")
+    private String randomValue;
+
+    @GetMapping("/simplemessage")
+    public ResponseEntity<SimpleMessage> getSimpleMessage(){
+        SimpleMessage simpleMessage = new SimpleMessage(randomValue);
+
+        return new ResponseEntity<>(simpleMessage, HttpStatus.OK);
+    }
 
     @PostMapping("/add")
     public ResponseEntity<PersonDTO> addPerson(@RequestBody PersonDTO personDto){
