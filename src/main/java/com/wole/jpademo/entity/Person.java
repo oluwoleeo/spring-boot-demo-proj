452fbd917600541302f97aaa6f2f18f06fd5dfa6
Oluwaremi Oluwole <polymathwole@gmail.com>
2023-10-05 07:24:08 +0100

    Remove Person entity package import

--- src/main/java/com/wole/jpademo/entity/Person.java
+++ src/main/java/com/wole/jpademo/entity/Person.java
@@ -1,6 +1,12 @@
 package com.wole.jpademo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
